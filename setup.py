b8ef8499e35b5ccda68ed171a69458f7fbfd2c86
Guido A.J. Stevens <guido.stevens@cosent.nl>
2014-11-15 07:23:00 +0100

    fixup setup.py, depend plonesocial.suite

--- setup.py
+++ setup.py
@@ -3,36 +3,36 @@ import os
 
 version = '0.1'
 
-setup(name='ploneintranet.pagerank',
-      version=version,
-      description="Personalized pagerank for Plone intranet",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='ploneintranet.pagerank',
+    version=version,
+    description="Personalized pagerank for Plone intranet",
+    long_description=open("README.txt").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['ploneintranet'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
-      )
+    ],
+    keywords='',
+    author='Guido A.J. Stevens',
+    author_email='guido.stevens@cosent.net',
+    url='https://github.com/ploneintranet/ploneintranet.pagerank',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['ploneintranet'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plonesocial.suite'
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
